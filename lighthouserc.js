cd9b5886841a8e0fa9608ffdd28d2b072fef4f22
Kostas Tseronis <k.tseronis@gmail.com>
2023-10-29 11:17:16 +0000

    upload to lhci server

--- lighthouserc.js
+++ lighthouserc.js
@@ -4,8 +4,22 @@ module.exports = {
             url: ['https://www.us.elsevierhealth.com'],
             numberOfRuns: 3,
         },
+        // upload: {
+        //     target: 'temporary-public-storage',
+        // },
         upload: {
-            target: 'temporary-public-storage',
+            target: 'lhci',
+            serverBaseUrl: 'http://54.76.206.129:9001',
+            token: '924fb67e-e07d-400b-8a9a-0e918f6d194e', // <-- Your project-specific token here
         },
+        "assert": {
+            "assertions": {
+                "categories:performance": ["error", { "minScore": 0.1 }],
+                "categories:accessibility": ["error", { "minScore": 0.1 }],
+                "categories:best-practices": ["warn", { "minScore": 1 }],
+                "categories:seo": ["error", { "minScore": 0.1 }],
+                "categories:pwa": ["warn", { "minScore": 0.99 }]
+            }
+        }
     },
 };
